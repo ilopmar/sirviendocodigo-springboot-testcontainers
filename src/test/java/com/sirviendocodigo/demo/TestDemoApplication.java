320af24dc7ce4aa1fbff36212fd9d619382b6835
Iván López <lopez.ivan@gmail.com>
2024-10-06 11:37:47 +0200

    Initial commit

---
+++ src/test/java/com/sirviendocodigo/demo/TestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sirviendocodigo.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(DemoApplication::main)
+        .with(TestcontainersConfiguration.class)
+        .run(args);
+  }
+
+}
